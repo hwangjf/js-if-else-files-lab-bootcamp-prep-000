6cb38a350cfa7a3472669717113a403ff5ac46c9
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-13 03:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var height = "70";
 module.exports = { name, height
 };
 
-if (name === "Susan") {
-    console.log("The name is correct");
-} else {
-  console.log("Expected the name to be Susan");
-}
+// if (name === "Susan") {
+//     console.log("The name is correct");
+// } else {
+//   console.log("Expected the name to be Susan");
+// }
 
 
 
