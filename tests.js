ce7af802056997a7ea584a1b414420a2fc1aba9a
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-13 03:02:44 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (name === "Susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -8,7 +8,7 @@ if (name === "Susan") {
 
 
 
-if (height === 74) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 74, Received: "+index.height)
